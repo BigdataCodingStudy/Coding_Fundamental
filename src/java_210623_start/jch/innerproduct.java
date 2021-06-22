b68dc32a91ab167fa0e394f8d5b32ac335167487
학생 <@DESKTOP-J1NIFGB>
2021-06-22 17:12:42 +0900

    프로그래머스-level1-내적-완료

---
+++ src/java_210623_start/jch/innerproduct.java
@@ -0,0 +1,28 @@
+package java_210623_start.jch;
+
+public class innerproduct {
+	
+	public int solution(int[] a, int[] b) {
+        int answer = 0;  
+        
+        int i = 0;
+        while(i<a.length) {
+        	answer += a[i]*b[i];
+        	i++;
+        }
+        
+        return answer;
+    
+	}
+	
+	
+
+
+	public static void main(String[] args) {
+		innerproduct ip = new innerproduct();
+		int[] c = { 1, 2, 3, 4 };
+		int[] d = { 1, 2, 3, 4 };
+		
+		System.out.println(ip.solution(c, d));
+	}
+}

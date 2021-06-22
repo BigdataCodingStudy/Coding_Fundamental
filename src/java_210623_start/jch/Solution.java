a8d1ee6e31461406c302be5b3758ea7dbebce5a1
학생 <@DESKTOP-J1NIFGB>
2021-06-22 16:08:07 +0900

    프로그래머스-level1-짝수와 홀수

--- src/java_210623_start/jch/Solution.java
+++ src/java_210623_start/jch/Solution.java
@@ -1,5 +1,18 @@
 package java_210623_start.jch;
 
 public class Solution {
-  
+	public String solution(int num) {
+        String answer = "";
+        if(num%2==0) {
+        	answer = "Even";
+        }else {
+        	answer = "Odd";
+        }
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println("the answer is : " +s.solution(5));
+	}
 }

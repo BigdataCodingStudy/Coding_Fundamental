614cc9da30c6ab1310bfff1167254eadff373c4d
youngwoo2020 <youngwoo2020@naver.com>
2021-06-30 12:30:11 +0900

    [권영우] 자릿수 더하기

---
+++ src/java_210630/kyw/Solution.java
@@ -0,0 +1,32 @@
+package java_210630.kyw;
+//자릿수 더하기
+public class Solution {
+	
+	public int solution(int n) {
+		int answer = 0;
+		while (n != 0) {
+			answer += n % 10;
+			n = n/10;
+		}
+		return answer;
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+	Solution s = new Solution();
+	System.out.println(s.solution(12345));
+	}
+}
+   /*
+    * 
+    * String num = String.valueOf(n);
+ 
+        for(int i = 0 ; i < num.length() ; i ++){
+            answer += Integer.parseInt(String.valueOf(num.charAt(i)));
+        }
+    * 
+    * 
+    * 
+    */

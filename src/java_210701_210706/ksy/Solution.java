b7222de90766d575f8507f49d291422a909973ee
학생 <@DESKTOP-GJHKAHJ>
2021-07-01 17:16:38 +0900

    20210701-ksy-가운데글자가져오기(완료)

--- src/java_210701_210706/ksy/Solution.java
+++ src/java_210701_210706/ksy/Solution.java
@@ -1,5 +1,30 @@
 package java_210701_210706.ksy;
 
+// 가운데 글자 가져오기
 public class Solution {
-
+	public String solution(String s) {
+		
+		int len = s.length();
+		return s.substring((len-1)/2, len/2+1);
+		
+		/*
+        String answer = "";
+        int len = s.length();
+        if(len%2==0) {
+    		answer = s.substring(len/2-1, len/2+1);
+    	}else {
+    		answer = Character.toString(s.charAt(len/2));
+    	}
+        System.out.println(answer);
+        return answer;
+        */
+    }
+	
+	
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.solution("abcd"));
+		
+//		s.solution("abcd");
+	}
 }

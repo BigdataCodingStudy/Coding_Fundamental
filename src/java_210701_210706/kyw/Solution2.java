404c472ebcb4c9f14bfc95bb581f094b4edde559
학생 <@DESKTOP-EP3BKJB>
2021-07-01 17:13:27 +0900

    [kyw]-내일은 되겠지..

---
+++ src/java_210701_210706/kyw/Solution2.java
@@ -0,0 +1,36 @@
+package java_210701_210706.kyw;
+// 자연수 뒤집어 배열
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution2 {
+	public int[] solution(long n) {
+        int[] answer = {};
+
+		String b = String.valueOf(n);
+		ArrayList list = new ArrayList();
+		
+		for (int i = b.length(); i >0; i--) {
+			list.add(b.substring(i-1,i));
+		}
+
+		answer = new int[list.size()];
+
+		for (int i = 0; i < list.size(); i++) {
+			answer[i] = (int) list.get(i);
+		}
+		
+        return answer;
+    }
+	
+	
+	
+	public static void main(String[] args) {
+		Solution2 s = new Solution2();
+		System.out.println(Arrays.toString(s.solution(9876)));
+		
+		
+	}
+
+}

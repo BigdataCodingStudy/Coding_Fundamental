711691581ad040b070c04ccdd865ba9afc215e60
ourus <ourus@DESKTOP-NBTVHAC>
2021-07-05 00:34:45 +0900

    [이재훈 7월 첫째주]2021-07-05

---
+++ src/java_210701_210706/ljh/Solution4.java
@@ -0,0 +1,24 @@
+package java_210701_210706.ljh;
+//가운데 글자 가져오기
+/*
+ * 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 
+ * 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+ */
+public class Solution4 {
+    public String solution(String s) {
+        String answer = "";
+        
+        String[] word = new String[s.length()]; //한글자씩 word 배열에 저장
+        word = s.split("");
+        
+        if(word.length%2==0){   //글자수가 짝수면
+            answer = word[(word.length/2) - 1] + word[word.length/2];
+        }
+        else{                   //글자수가 홀수면
+            answer = word[word.length/2];
+        }
+        
+        
+        return answer;
+    }
+}

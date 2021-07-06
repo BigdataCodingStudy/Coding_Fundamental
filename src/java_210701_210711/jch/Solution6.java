f73a94f7f904c138f0183d9647c6268f68350977
학생 <@DESKTOP-J1NIFGB>
2021-07-06 14:49:13 +0900

    -미완료-프로그래머스-두개뽑아서더하기

---
+++ src/java_210701_210711/jch/Solution6.java
@@ -0,0 +1,25 @@
+package java_210701_210711.jch;
+
+import java.util.Arrays;
+
+class Solution6 {
+	public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        Arrays.sort(numbers);
+        
+        
+        
+        
+        return answer;
+    }
+    
+    
+    public static void main(String[] args) {
+		Solution6 s = new Solution6();
+		int[] numbers = {2,1,3,4,1};
+		int[] numbers2 = {5,0,2,7};
+		
+		System.out.println(s.solution(numbers));
+		System.out.println(s.solution(numbers2));
+	}
+}

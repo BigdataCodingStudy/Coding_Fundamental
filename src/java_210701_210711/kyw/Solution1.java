f2a11bd1a2251b1c2b32487e6e47ee69191e88b4
학생 <@DESKTOP-EP3BKJB>
2021-07-06 17:52:33 +0900

    [권영우]-소수만들기

--- src/java_210701_210711/kyw/Solution1.java
+++ src/java_210701_210711/kyw/Solution1.java
@@ -4,40 +4,46 @@ import java.util.ArrayList;
 
 //소수만들기
 public class Solution1 {
-	public static void main(String[] args) {
-		int[] a = { 1, 2, 7, 6, 4 };
-		ArrayList list = new ArrayList();
-		int[] answer = {};
-		int count = 0;
 
-		for (int i = 0; i < a.length; i++) {
+	public static boolean isprimed(int a) {
+		boolean isp = false;
+		for (int i = 2; i <= Math.sqrt(a); i++) {
+			if (a % i == 0) {
+				isp = true;
+			}
+		}
+
+		return isp;
+	}
+
+	public int solution(int[] nums) {
+		int answer = 0;
+
+		ArrayList<Integer> list = new ArrayList<>();
+
+		for (int i = 0; i < nums.length; i++) {
 			for (int j = 0; j < i; j++) {
 				for (int j2 = 0; j2 < j; j2++) {
-				
-					if (!list.contains(a[i] + a[j] + a[j2])) {
-						list.add(a[i] + a[j] + a[j2]);						
+
+					if (!list.contains(nums[i] + nums[j] + nums[j2])) {
+						if (isprimed(nums[i] + nums[j] + nums[j2]) == false) {
+							list.add(nums[i] + nums[j] + nums[j2]);
+							answer += 1;
+						}
+
 					}
 				}
 			}
 		}
 
-		System.out.println(list);
-		answer = new int[list.size()];
-		for (int i = 0; i < answer.length; i++) {
-			answer[i] = (int) list.get(i);
+		return answer;
+	}
 
-		}
+	public static void main(String[] args) {
+		Solution1 s2 = new Solution1();
+		int[] a = { 1, 2, 7, 6, 4 };
+		System.out.println(s2.solution(a));
 
-	/*	int x = 7;
-		for (int i = 2; i < x; i++) {
-			if (x % i == 0) {
-
-				count += 1;
-			}
-		}*/
-		System.out.println(count);
-		
-		
 	}
 
 }

beaa9fb012314cb2ff7766e348f9183945ac9fd3
학생 <@DESKTOP-J1NIFGB>
2021-07-06 17:02:11 +0900

    -완료-프로그래머스-두개뽑아서더하기

--- src/java_210701_210711/jch/Solution6.java
+++ src/java_210701_210711/jch/Solution6.java
@@ -1,16 +1,28 @@
 package java_210701_210711.jch;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 
 class Solution6 {
-	public int[] solution(int[] numbers) {
-        int[] answer = new int[numbers.length];
-        Arrays.sort(numbers);
+	public Integer[] solution(int[] numbers) {
         
+        ArrayList<Integer> list = new ArrayList<Integer>();
         
-        
-        
-        return answer;
+        for(int i=0; i<numbers.length-1; i++) {
+            for(int j=i+1; j<numbers.length; j++) {
+        			list.add(numbers[i]+numbers[j]);
+        	}
+        }
+        if(numbers.length == 1) list.add(0);
+		
+		// 배열을 HashSet으로 변환
+		HashSet<Integer> answerSet = new HashSet<>(list);
+		// HashSet을 배열로 변환
+		Integer[] resultAnswer = answerSet.toArray(new Integer[0]);
+		
+        Arrays.sort(resultAnswer);
+        return resultAnswer;
     }
     
     
@@ -18,8 +30,10 @@ class Solution6 {
 		Solution6 s = new Solution6();
 		int[] numbers = {2,1,3,4,1};
 		int[] numbers2 = {5,0,2,7};
+		int[] numbers3 = {0};
 		
-		System.out.println(s.solution(numbers));
-		System.out.println(s.solution(numbers2));
+		System.out.println(Arrays.toString(s.solution(numbers)));
+		System.out.println(Arrays.toString(s.solution(numbers2)));
+		System.out.println(Arrays.toString(s.solution(numbers3)));
 	}
 }

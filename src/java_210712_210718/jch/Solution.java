4d3ec07bb3966ae7eb271ca539cab6bea54ff0ee
CHOHEEJEONG <chohee0505@gmail.com>
2021-07-12 12:52:39 +0900

    -완료-프로그래머스-2016

--- src/java_210712_210718/jch/Solution.java
+++ src/java_210712_210718/jch/Solution.java
@@ -1,5 +1,40 @@
 package java_210712_210718.jch;
 
 public class Solution {
+	public String solution(int a, int b) {
+        String answer = "";
+        //2016년 1월 1일은 금요일
+        int year = 2016;
+        
+        int[] monthArray = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        boolean isLeafYear = year%4==0 && year%100!=0 || year%400==0;
 
+        if(isLeafYear) {
+        	monthArray[1] =29;
+        }
+        
+        String []dayArray = {"THU", "FRI", "SAT", "SUN","MON","TUE","WED"};
+        
+        int totalCount = 0;
+        for(int i =0 ; i<a-1 ; i++) {
+        	totalCount += monthArray[i];
+        }
+        totalCount += b;
+        
+        for(int i =0; i<dayArray.length; i++) {
+        	if(totalCount%7==i) {
+        		answer = dayArray[i];
+        	}
+        	
+        }
+       
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		int a = 1;
+		int b= 2;
+		System.out.println(s.solution(a, b));
+	}
 }

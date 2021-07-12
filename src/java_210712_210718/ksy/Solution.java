585f8b74bd17890f6384356931577b03859b035a
ssu0818 <sykim1336@naver.com>
2021-07-12 12:59:52 +0900

    2021-07-12-ksy-자릿수 더하기(완료)

--- src/java_210712_210718/ksy/Solution.java
+++ src/java_210712_210718/ksy/Solution.java
@@ -1,5 +1,38 @@
 package java_210712_210718.ksy;
 
-public class Solution {
+import java.util.Scanner;
 
+// ﻿자릿수 더하기
+public class Solution {
+	
+	public int solution(int n) {
+        int answer = 0;
+        
+        
+        /*
+        String s = String.valueOf(n);
+        
+        for(int i=0; i<s.length(); i++) {
+        	answer += Integer.parseInt(s.substring(i, i+1));
+        }
+         */
+        
+        while(n>0) {
+        	answer += n%10;
+        	n/=10;
+        }
+        
+        System.out.println(answer);
+        return answer;
+    }
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요 :D");
+		int n = sc.nextInt();
+		Solution s = new Solution();
+		s.solution(n);
+	}
+	
 }

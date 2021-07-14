ced7d39e7d766bfa35ddd5d2a8683afff3edbf23
ssu0818 <sykim1336@naver.com>
2021-07-14 18:21:29 +0900

    2021-07-14-ksy-직사각형 별찍기(완료)

---
+++ src/java_210712_210718/ksy/Solution5.java
@@ -0,0 +1,28 @@
+package java_210712_210718.ksy;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+// 직사각형 별찍기
+public class Solution5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("count: ");
+        int a = sc.nextInt();
+        System.out.println("line: ");
+        int b = sc.nextInt();
+        
+        StringBuilder sb = new StringBuilder();
+        IntStream.range(0, a).forEach(s -> sb.append("*"));
+        IntStream.range(0, b).forEach(s -> System.out.println(sb.toString()));
+        
+        /*
+        for(int i=0; i<b; i++) {
+        	for(int j=0; j<a; j++) {
+        		System.out.print("*");
+        	}
+        	System.out.println();
+        }
+        */
+    }
+}

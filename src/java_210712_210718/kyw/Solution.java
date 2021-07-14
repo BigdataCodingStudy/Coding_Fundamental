d6e9c457a2db23478a856f81848f1b36f5e9742a
youngwoo2020 <youngwoo2020@naver.com>
2021-07-14 11:33:03 +0900

    [권영우]-k번째수 완료

--- src/java_210712_210718/kyw/Solution.java
+++ src/java_210712_210718/kyw/Solution.java
@@ -1,5 +1,33 @@
 package java_210712_210718.kyw;
+//k번째수
+import java.util.ArrayList;
 
-public class Solution {
+import java.util.Collections;
 
+class Solution {
+    
+	public int sub(int[]array,int[][]commands, int n){
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int sub = 0;
+		for (int i = commands[n][0]-1; i < commands[n][1]; i++) {	
+			list.add(array[i]);
+			Collections.sort(list);
+		}
+		sub=(list.get(commands[n][2]-1));
+		return sub;
+		}
+    
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
+          ArrayList<Integer> answers =new ArrayList<Integer>();
+	    	
+	    	for (int n = 0; n < commands.length; n++) {
+	    		answers.add(sub(array, commands, n));
+	    	}
+	    	answer = new int[answers.size()];
+	    	for (int i = 0; i < answer.length; i++) {
+				answer[i]=answers.get(i);
+			}
+        return answer;
+    }
 }

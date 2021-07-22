38f787b1807eea4316658dfab878747084765013
ourus <ourus@DESKTOP-NBTVHAC>
2021-07-23 08:59:47 +0900

    [이재훈]2021-07-23 직사각형 별찍기

--- src/java_210719_210725/ljh/Solution.java
+++ src/java_210719_210725/ljh/Solution.java
@@ -1,5 +1,20 @@
 package java_210719_210725.ljh;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        for(int i=0; i<b; i++){
+            for(int j=0; j<a; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //System.out.println(a + b);
+    }
 }

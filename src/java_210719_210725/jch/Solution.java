e4f501355b65cefc7d23d7c23cbee0a90bce3ff9
CHOHEEJEONG <chohee0505@gmail.com>
2021-07-24 11:31:38 +0900

    -완료-개인-피라미드 쌓기

--- src/java_210719_210725/jch/Solution.java
+++ src/java_210719_210725/jch/Solution.java
@@ -1,5 +1,20 @@
 package java_210719_210725.jch;
 
 public class Solution {
-
+	public static void main(String[] args) {
+		int k = 1;
+		
+		for(int i=1; i<=10; i++) {
+			while(k<=10-i) {
+				System.out.print(" ");
+				k++;
+			}
+			k = 1;
+			for(int j=1; j<=2*i-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+	}
 }
